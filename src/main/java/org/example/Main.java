434b1412903c90ec05528cae0cd1fdd134261ecc
Umid <umidjan9399@gmail.com>
2023-03-04 20:59:23 +0500

    O'zgarish

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Mazgi");
+        System.out.println("Lashara");
     }
 }
